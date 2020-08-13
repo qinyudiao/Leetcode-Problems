7db88d4fad61bf8ba1a564761c9251b1b255d676
qinyudiao <qdiao@utexas.edu>
2020-08-13 16:00:52 -0500

    Update Problem 382

--- src-medium/linked_list_random_node_382/ListNode.java
+++ src-medium/linked_list_random_node_382/ListNode.java
@@ -1,10 +1,5 @@
 package linked_list_random_node_382;
 
-//	Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
-//
-//	Follow up:
-//	What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
-
 public class ListNode {
     int val;
     ListNode next;
--- src-medium/linked_list_random_node_382/Solution.java
+++ src-medium/linked_list_random_node_382/Solution.java
@@ -6,6 +6,10 @@ import java.util.Random;
 //	return a random node's value from the linked list. 
 //	Each node must have the same probability of being chosen.
 
+//	Follow up:
+//	What if the linked list is extremely large and its length is unknown to you? 
+//  Could you solve this efficiently without using extra space?
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
