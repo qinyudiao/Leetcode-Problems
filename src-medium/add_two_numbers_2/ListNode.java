3edbc84f376e2476b594b04fc232b66a61480d31
qinyudiao <qdiao@utexas.edu>
2020-08-21 17:23:06 -0500

    Remove ListNode Class

--- src-medium/add_two_numbers_2/ListNode.java
+++
@@ -1,9 +0,0 @@
-package add_two_numbers_2;
-
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-	ListNode(int val) { this.val = val; }
-	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
