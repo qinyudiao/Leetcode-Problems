2c750937f47953eb4fae15d7e41aab92f7627713
qinyudiao <qdiao@utexas.edu>
2020-08-24 13:45:56 -0500

    Add Problem 208

---
+++ src-medium/implement_trie_prefix_tree_208/Main.java
@@ -0,0 +1,14 @@
+package implement_trie_prefix_tree_208;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Trie trie = new Trie();
+		trie.insert("apple");
+		System.out.println(trie.search("apple"));   // returns true
+		System.out.println(trie.search("app"));     // returns false
+		System.out.println(trie.startsWith("app")); // returns true
+		trie.insert("app");  
+		System.out.println(trie.search("app"));     // returns true
+	}
+}
---
+++ src-medium/implement_trie_prefix_tree_208/Trie.java
@@ -0,0 +1,65 @@
+package implement_trie_prefix_tree_208;
+
+public class Trie {
+	TrieNode root;
+	
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+    	TrieNode curr = root;
+		
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+	        if (curr.children[c - 'a'] == null) {
+	          curr.children[c - 'a'] = new TrieNode(); 
+	        }
+	        curr = curr.children[c - 'a'];  
+        }
+		
+		curr.isCompleteWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+    	TrieNode curr = root;
+        for(int i = 0; i < word.length(); i++) {
+        	char c = word.charAt(i);
+        	if(curr.children[c - 'a'] == null)
+        		return false;
+        	curr = curr.children[c - 'a'];  
+        }
+        
+        return curr.isCompleteWord ? true : false;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+    	TrieNode curr = root;
+        for(int i = 0; i < prefix.length(); i++) {
+        	char c = prefix.charAt(i);
+        	if(curr.children[c - 'a'] == null)
+        		return false;
+        	curr = curr.children[c - 'a'];  
+        }
+        
+        return true;
+    }
+}
+
+class TrieNode {
+	
+	public TrieNode[] children;
+	public boolean isCompleteWord;
+	
+	public TrieNode() {
+		children = new TrieNode[26];
+    	isCompleteWord = false;
+	}
+}
+
+//	Runtime: 30 ms, faster than 92.79% of Java online submissions for Implement Trie (Prefix Tree).
+//	Memory Usage: 48.9 MB, less than 79.34% of Java online submissions for Implement Trie (Prefix Tree).
