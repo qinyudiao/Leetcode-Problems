ebb63ad899d7190ab40cc09f057168ca56c610f5
qinyudiao <qdiao@utexas.edu>
2020-08-31 18:12:44 -0500

    Update TreeNode

--- src-utilities/utilities/TreeNode.java
+++ src-utilities/utilities/TreeNode.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreeNode {
 	
 	public int val;
@@ -16,6 +19,47 @@ public class TreeNode {
     	this.val = val;
     	this.left = left;
     	this.right = right;
+    }
+    
+    public String deepToString() {
+    	List<Integer> vals = new ArrayList<>();
+    	
+    	List<TreeNode> toVisit = new ArrayList<>();
+    	List<TreeNode> toVisitNext = new ArrayList<>();
+    	toVisit.add(this);
+    	while(!toVisit.isEmpty()) {
+    		boolean allNull = true;
+    		for(TreeNode node : toVisit) {
+    			if(node == null) {
+    				vals.add(null);
+    				toVisitNext.add(null);
+    				toVisitNext.add(null);
+    			}
+    			else {
+    				vals.add(node.val);
+    				if(node.left != null) {
+    					toVisitNext.add(node.left);
+    					allNull = false;
+    				}
+    				else 
+    					toVisitNext.add(null);
+    				
+    				if(node.right != null) {
+    					toVisitNext.add(node.right);
+    					allNull = false;
+    				}
+    				else 
+    					toVisitNext.add(null);
+    			}
+    		}
+    		toVisit.clear();
+    		if(!allNull) {
+    			toVisit.addAll(toVisitNext);
+    			toVisitNext.clear();
+    		}
+    	}
+    	
+		return vals.toString();
     }
 }
  
