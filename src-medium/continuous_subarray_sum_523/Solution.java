e5fb65970882fc7bd15ce0fd316100b11c495238
Jack Diao <qdiao@utexas.edu>
2020-09-23 23:10:38 -0500

    Fix Indentation

--- src-medium/continuous_subarray_sum_523/Solution.java
+++ src-medium/continuous_subarray_sum_523/Solution.java
@@ -21,68 +21,70 @@ public class Solution {
 	
 	// use HashMap to save time
 	public boolean checkSubarraySum(int[] nums, int k) {
-        int sum = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-        for(int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (k != 0)
-                sum = sum % k;
-            if (map.containsKey(sum)) {
-                if (i - map.get(sum) > 1)
-                    return true;
-            } else
-                map.put(sum, i);
-        }
-        return false;
-    }
+		int sum = 0;
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(0, -1);
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if (k != 0)
+				sum = sum % k;
+			if (map.containsKey(sum)) {
+				if (i - map.get(sum) > 1)
+					return true;
+			}
+			else
+				map.put(sum, i);
+		}
+		
+		return false;
+	}
 	
 	// dp[n] // n is the end index
 	public boolean checkSubarraySum1(int[] nums, int k) {
 		int n = nums.length;
-        int[] dp = new int[n+1];
-        
-        dp[1] = nums[0];
-        for(int l = 2; l <= n; l++) {
-        	for(int i = l; i <= n; i++) {
-        		dp[i] = (i == l) ? dp[i-1] + nums[i-1] : dp[i-1] + nums[i-1] - nums[i-l-1];
-//        		System.out.print(dp[i] + " ");
-//        		System.out.print(i + " ");
-        		if(k == 0) {
-            		if(dp[i] == 0) {
-            			System.out.print(i - l + ", " + i + " ");
-            			return true;
-            		}
-            	}
-            	else if(dp[i] % k == 0) {
-            		System.out.print(i - l + ", " + i + " ");
-            		return true;
-            	}
-        	}
-        }
+		int[] dp = new int[n+1];
+
+		dp[1] = nums[0];
+		for(int l = 2; l <= n; l++) {
+			for(int i = l; i <= n; i++) {
+				dp[i] = (i == l) ? dp[i-1] + nums[i-1] : dp[i-1] + nums[i-1] - nums[i-l-1];
+	//        		System.out.print(dp[i] + " ");
+	//        		System.out.print(i + " ");
+				if(k == 0) {
+					if(dp[i] == 0) {
+						System.out.print(i - l + ", " + i + " ");
+						return true;
+					}
+				}
+				else if(dp[i] % k == 0) {
+					System.out.print(i - l + ", " + i + " ");
+					return true;
+				}
+			}
+		}
 		return false;
-    }
+	}
 
 	// works, but memory limit exceeded;
 	public boolean checkSubarraySum2(int[] nums, int k) {
 		int n = nums.length;
-        int[][] dp = new int[n][n+1];
-        
-        for(int i = 0; i < n; i++) {
-        	for(int j = i + 1; j <= n; j++) {
-            	dp[i][j] = dp[i][j-1] + nums[j-1];
-            	if(j - i >= 2) {
-	            	if(k == 0) {
-	            		if(dp[i][j] == 0)
-	            			return true;
-	            	}
-	            	else if(dp[i][j] % k == 0) {
-	            		System.out.print(i + ", " + j + " ");
-	            		return true;
-	            	}
-            	}
-            }
-        }
+		int[][] dp = new int[n][n+1];
+
+		for(int i = 0; i < n; i++) {
+			for(int j = i + 1; j <= n; j++) {
+				dp[i][j] = dp[i][j-1] + nums[j-1];
+				if(j - i >= 2) {
+					if(k == 0) {
+						if(dp[i][j] == 0)
+							return true;
+					}
+					else if(dp[i][j] % k == 0) {
+						System.out.print(i + ", " + j + " ");
+						return true;
+					}
+				}
+			}
+		}
 		return false;
-    }
+	}
 }
