99eb724d13a7c52c093e64786e934fb7ed8a7dae
Jack Diao <qdiao@utexas.edu>
2020-09-23 22:02:09 -0500

    Fix Indentation

--- src-easy/word_pattern_290/Solution.java
+++ src-easy/word_pattern_290/Solution.java
@@ -34,20 +34,20 @@ public class Solution {
 		
 		Map<Character, String> mapChar = new HashMap<>(); // Space complexity: O(1), because there can only be 26 lowercase letters.
 		Map<String, Character> mapWord = new HashMap<>(); // Space complexity: O(1), or O(m), where m is less than 26.
-        for(int i = 0; i < pattern.length(); i++) {
-        	if(mapChar.containsKey(pattern.charAt(i))) {
-        		if(!mapChar.get(pattern.charAt(i)).equals(words[i]))
-        			return false;
-        	}
-        	else if(mapWord.containsKey(words[i]))
-        		return false;
-        	else {
-        		mapChar.put(pattern.charAt(i), words[i]);
-        		mapWord.put(words[i], pattern.charAt(i));
-        	}	
-        }
+		for(int i = 0; i < pattern.length(); i++) {
+			if(mapChar.containsKey(pattern.charAt(i))) {
+				if(!mapChar.get(pattern.charAt(i)).equals(words[i]))
+					return false;
+			}
+			else if(mapWord.containsKey(words[i]))
+				return false;
+			else {
+				mapChar.put(pattern.charAt(i), words[i]);
+				mapWord.put(words[i], pattern.charAt(i));
+			}	
+		}
 		return true;
-    }
+	}
 	
 	//	Runtime: 0 ms, faster than 100.00% of Java online submissions for Word Pattern.
 	//	Memory Usage: 37.3 MB, less than 86.26% of Java online submissions for Word Pattern. 
@@ -60,19 +60,19 @@ public class Solution {
 			return false;
 		
 		Map<Character, String> map = new HashMap<>(); // Space complexity: O(1)
-        for(int i = 0; i < pattern.length(); i++) {
-        	if(map.containsKey(pattern.charAt(i))) {
-        		if(!map.get(pattern.charAt(i)).equals(words[i]))
-        			return false;
-        	}
-        	else if(map.containsValue(words[i])) // containsValues() still takes O(1) because there are at most 26 values in the map.
-        		return false;
-        	else {
-        		map.put(pattern.charAt(i), words[i]);
-        	}	
-        }
+		for(int i = 0; i < pattern.length(); i++) {
+			if(map.containsKey(pattern.charAt(i))) {
+				if(!map.get(pattern.charAt(i)).equals(words[i]))
+					return false;
+			}
+			else if(map.containsValue(words[i])) // containsValues() still takes O(1) because there are at most 26 values in the map.
+				return false;
+			else {
+				map.put(pattern.charAt(i), words[i]);
+			}	
+		}
 		return true;
-    }
+	}
 
 	//	Runtime: 0 ms, faster than 100.00% of Java online submissions for Word Pattern.
 	//	Memory Usage: 37.1 MB, less than 97.50% of Java online submissions for Word Pattern.
