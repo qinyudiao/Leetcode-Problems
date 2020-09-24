9f4b181df458156773e8696d5bf969d033ec08c0
Jack Diao <qdiao@utexas.edu>
2020-09-23 22:34:50 -0500

    Fix Indentation

--- src-medium/unique_paths_62/Solution.java
+++ src-medium/unique_paths_62/Solution.java
@@ -24,23 +24,23 @@ public class Solution {
 	// T(n) = O(m*n)
 	// S(n) = O(m*n)
 	public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int[] row : dp) {
-        	row[0] = 1;
-        }
-        for(int j = 1; j < n; j++) {
-        	dp[0][j] = 1;
-        }
-        for(int i = 1; i < m; i++) {
-        	for(int j = 1; j < n; j++) {
-            	dp[i][j] = dp[i][j-1] + dp[i-1][j];
-        	}
-        }
-        
-        System.out.println(Arrays.deepToString(dp));
-        
+		int[][] dp = new int[m][n];
+		for(int[] row : dp) {
+			row[0] = 1;
+		}
+		for(int j = 1; j < n; j++) {
+			dp[0][j] = 1;
+		}
+		for(int i = 1; i < m; i++) {
+			for(int j = 1; j < n; j++) {
+			dp[i][j] = dp[i][j-1] + dp[i-1][j];
+			}
+		}
+
+		System.out.println(Arrays.deepToString(dp));
+
 		return dp[m-1][n-1];
-    }
+	}
 }
 
 //	Runtime: 0 ms, faster than 100.00% of Java online submissions for Unique Paths.
