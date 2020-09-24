1272cb1e09dcb6f7114f3059fbd7330c4ee7d307
Jack Diao <qdiao@utexas.edu>
2020-09-23 22:40:39 -0500

    Fix Indentation

--- src-medium/linked_list_random_node_382/Solution.java
+++ src-medium/linked_list_random_node_382/Solution.java
@@ -38,31 +38,33 @@ public class Solution {
 		System.out.println(count/100 + "%");
 	}
 
-    ListNode head;
-    /** @param head The linked list's head.
-        Note that the head is guaranteed to be not null, so it contains at least one node. */
-    public Solution(ListNode head) {
-        this.head = head;
-    }
+	ListNode head;
+	/** @param head The linked list's head.
+		Note that the head is guaranteed to be not null, so it contains at least one node. 
+	*/
+	
+	public Solution(ListNode head) {
+        	this.head = head;
+	}
     
-    /** Returns a random node's value. */
-    public int getRandom() {
-        int size = 1;
-        ListNode temp = head;
-        while(temp.next != null){
-            temp = temp.next;
-            size++;
-        }
-        Random rand = new Random(); 
-        int random_num = rand.nextInt(size);
-        temp = head;
-        int count = 0;
-        while(count<random_num){
-            temp = temp.next;
-            count++;
-        }
-        return temp.val;
-    }
+	/** Returns a random node's value. */
+	public int getRandom() {
+		int size = 1;
+		ListNode temp = head;
+		while(temp.next != null){
+			temp = temp.next;
+			size++;
+		}
+		Random rand = new Random(); 
+		int random_num = rand.nextInt(size);
+		temp = head;
+		int count = 0;
+		while(count<random_num){
+			temp = temp.next;
+			count++;
+		}
+		return temp.val;
+	}
 }
 
 /**
