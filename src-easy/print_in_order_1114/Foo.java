f953351398cf87cb1c6e48bb98e0d1c3699a4704
Jack Diao <qdiao@utexas.edu>
2020-09-23 22:13:32 -0500

    Fix Indentation

--- src-easy/print_in_order_1114/Foo.java
+++ src-easy/print_in_order_1114/Foo.java
@@ -2,28 +2,28 @@ package print_in_order_1114;
 
 class Foo {
 
-    public volatile int threadOrderSemaphore;
-    
-    public Foo() {
-        threadOrderSemaphore = 0;
-    }
+        public volatile int threadOrderSemaphore;
 
-    public void first(Runnable printFirst) throws InterruptedException {
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-        threadOrderSemaphore++;
-    }
+        public Foo() {
+            threadOrderSemaphore = 0;
+        }
 
-    public void second(Runnable printSecond) throws InterruptedException {
-        while(threadOrderSemaphore != 1);
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-        threadOrderSemaphore++;
-    }
+        public void first(Runnable printFirst) throws InterruptedException {
+                // printFirst.run() outputs "first". Do not change or remove this line.
+                printFirst.run();
+                threadOrderSemaphore++;
+        }
 
-    public void third(Runnable printThird) throws InterruptedException {
-        while(threadOrderSemaphore != 2);
-        // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
-    }
+        public void second(Runnable printSecond) throws InterruptedException {
+                while(threadOrderSemaphore != 1);
+                // printSecond.run() outputs "second". Do not change or remove this line.
+                printSecond.run();
+                threadOrderSemaphore++;
+        }
+
+        public void third(Runnable printThird) throws InterruptedException {
+                while(threadOrderSemaphore != 2);
+                // printThird.run() outputs "third". Do not change or remove this line.
+                printThird.run();
+        }
 }
