0ad5ca01ae1c041b1f2bfa4f8014802b9b6159c7
Jack Diao <qdiao@utexas.edu>
2020-09-23 22:34:11 -0500

    Fix Indentation

--- src-medium/unique_paths_II_63/Solution.java
+++ src-medium/unique_paths_II_63/Solution.java
@@ -31,33 +31,33 @@ public class Solution {
 	// T(n) = O(m*n)
 	// S(n) = O(m*n)
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        
-        int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++) {
-        	if(obstacleGrid[i][0] > 0)
-        		break;
-        	dp[i][0] = 1;
-        }
-        for(int j = 0; j < n; j++) {
-        	if(obstacleGrid[0][j] > 0)
-        		break;
-        	dp[0][j] = 1;
-        }
-        for(int i = 1; i < m; i++) {
-        	for(int j = 1; j < n; j++) {
-        		if(obstacleGrid[i][j] > 0)
-        			dp[i][j] = 0;
-        		else
-        			dp[i][j] = dp[i][j-1] + dp[i-1][j];
-        	}
-        }
-        
-        System.out.println(Arrays.deepToString(dp));
+		int m = obstacleGrid.length;
+		int n = obstacleGrid[0].length;
+
+		int[][] dp = new int[m][n];
+		for(int i = 0; i < m; i++) {
+			if(obstacleGrid[i][0] > 0)
+				break;
+			dp[i][0] = 1;
+		}
+		for(int j = 0; j < n; j++) {
+			if(obstacleGrid[0][j] > 0)
+				break;
+			dp[0][j] = 1;
+		}
+		for(int i = 1; i < m; i++) {
+			for(int j = 1; j < n; j++) {
+				if(obstacleGrid[i][j] > 0)
+					dp[i][j] = 0;
+				else
+					dp[i][j] = dp[i][j-1] + dp[i-1][j];
+			}
+		}
+
+		System.out.println(Arrays.deepToString(dp));
         
 		return dp[m-1][n-1];
-    }
+	}
 }
 
 //	Runtime: 0 ms, faster than 100.00% of Java online submissions for Unique Paths II.
